34f6634b2c812cb3c89ad242ac9afb933edb3bd5
Jeneel Frani <jeneel@MacBook-Pro-3.local>
2024-11-14 20:50:06 -0700

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    // implementation: 'sass-embedded',
+    // includePaths: [__dirname + '/src/sass/**/*.scss'],
+  }
 };
 
 export default nextConfig;
---
+++ src/app/components/Fretboard/Fretboard.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import styles from './Fretboard.module.scss'
+
+type Props = {
+    frets: number,
+    tuning: string[],
+}
+
+const Fretboard = ({ frets, tuning }: Props) => {
+
+    const createChromaticScale = (startingNote: string, length: number) => {
+        const chromaticScale = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+        const startingNoteIndex = chromaticScale.indexOf(startingNote.toUpperCase())
+
+        return Array.from({ length }, (_, i) => chromaticScale[(startingNoteIndex + i) % chromaticScale.length])
+    }
+
+    const stringNotes = tuning.map((note) => {
+
+        return createChromaticScale(note, frets + 1)
+    })
+
+    return (
+        <div className='w-full flex flex-row'>
+            <div className="flex flex-col-reverse gap-2">
+                {
+                    tuning.map((note, index) => {
+                        return (
+                            <div key={`${note}-${index}`} className={`${styles.fret} w-1/12 flex flex-col gap-2 border-l border-white`}>
+                                <div className="relative flex items-center justify-center border-white">
+                                    <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-1 bg-white"></span>
+                                    <div className="relative flex p-3 py-1 rounded-[100%] bg-slate-800">
+                                        <span className="text-white">
+                                            {note}
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="w-full flex flex-col">
+                {
+                    stringNotes.map((notes, index) => {
+                        return (
+                            <div key={index} className="flex flex-row grow">
+                                {
+                                    notes.map((note, index) => {
+                                        return (
+                                            <div key={`${note}-${index}`} className="note relative flex items-center justify-center grow border-l">
+                                                <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-[1px] bg-white"></span>
+                                                <div className="relative w-6 h-6 flex items-center justify-center p-3 py-1 rounded-[100%] bg-slate-800">
+                                                    <div className="text-white text-[10px]">
+                                                        {note}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        )
+                        // return (
+                        //     <div key={index} className={`${styles.fret} w-1/12 flex flex-col-reverse gap-2 border-l-2 border-white`}>
+                        //         {
+                        //             strings.map((string, index) => {
+                        //                 return (
+                        //                     <div key={`${fret}-${index}`} className="relative flex items-center justify-center border-white">
+                        //                         <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-1 bg-white"></span>
+                        //                         <div className="relative flex p-3 py-1 rounded-[100%] bg-slate-800">
+                        //                             <span className="text-white">
+                        //                                 {string}
+                        //                             </span>
+                        //                         </div>
+                        //                     </div>
+                        //                 )
+                        //             })
+                        //         }
+                        //     </div>
+                        // )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Fretboard
